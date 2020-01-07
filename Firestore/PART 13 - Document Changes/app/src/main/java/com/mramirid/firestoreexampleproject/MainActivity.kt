d37604dbc41d6123912560422e140394c853acfc
Amir <amir.rhythm@gmail.com>
2020-01-07 16:21:13 +0700

    Added PART 13

---
+++ Firestore/PART 13 - Document Changes/app/src/main/java/com/mramirid/firestoreexampleproject/MainActivity.kt
@@ -0,0 +1,106 @@
+package com.mramirid.firestoreexampleproject
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.DocumentChange.Type.*
+import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val database = FirebaseFirestore.getInstance()
+    // Referensi ke collection Notebook
+    private val notebookReference = database.collection("Notebook")
+
+    // Untuk pagination, menandai dokumen lanjutan (checkpoint)
+    private lateinit var lastResult: DocumentSnapshot
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        notebookReference.addSnapshotListener(this) { queryDocumentSnapshots, exception ->
+            if (exception != null) {
+                return@addSnapshotListener
+            }
+
+            for (documentChange in queryDocumentSnapshots?.documentChanges!!) {
+                val documentSnapshot = documentChange.document
+
+                val id = documentSnapshot.id
+                val oldIndex = documentChange.oldIndex // Ketika baru menambah oldIndex bernilai -1
+                val newIndex = documentChange.newIndex // Ketika menghapus newIndex bernilai -1
+
+                when(documentChange.type) {
+                    ADDED -> tv_data.append(
+                        getString(R.string.indexChange, "Added", id, oldIndex, newIndex)
+                    )
+                    MODIFIED -> tv_data.append(
+                        getString(R.string.indexChange, "Modified", id, oldIndex, newIndex)
+                    )
+                    REMOVED -> tv_data.append(
+                        getString(R.string.indexChange, "Removed", id, oldIndex, newIndex)
+                    )
+                }
+            }
+        }
+    }
+
+    fun addNote(view: View) {
+        val title = edt_title.text.toString()
+        val description = edt_description.text.toString()
+
+        if (edt_priority.length() == 0) {
+            edt_priority.setText("0")
+        }
+
+        val priority = edt_priority.text.toString().toInt()
+
+        val note = Note(title, description, priority)
+
+        // Menambah data di firestore, bisa ditambahi listener addOnSuccess dan addOnFailure di akhir
+        notebookReference.add(note)
+    }
+
+    fun loadNotes(view: View) {
+//        notebookReference.orderBy("priority")
+//            .orderBy("title")
+//            .startAt(3, "Title2") // Dimulai dari priority = 3 dengan title = Title2
+//            .startAt(3) // Dimulai pada priority = 3
+
+        val query = if (!this::lastResult.isInitialized) {
+            // Jika lastResult belum terisi berati kita belum menerima data sekalipun
+            notebookReference.orderBy("priority")
+                    .limit(3)
+            } else {
+                notebookReference.orderBy("priority")
+                    .startAfter(lastResult) // Jika lastResult ada, gunakan sebagai checkpoint
+                    .limit(3)
+            }
+
+        query.get()
+            .addOnSuccessListener { queryDocumentSnapshots ->
+                var data = ""
+
+                for (documentSnapshot in queryDocumentSnapshots) {
+                    val note = documentSnapshot.toObject(Note::class.java)
+                    note.documentId = documentSnapshot.id
+
+                    data += getString(R.string.loadResult, note.documentId, note.title, note.description, note.priority)
+                }
+
+                if (queryDocumentSnapshots.size() > 0) { // pagination dilakukan jika ada data
+                    data += "---------------\n\n"   // Pemisah paging
+                    tv_data.append(data)
+
+                    // Simpan dokumen yang terakhir didapat (checkpoint)
+                    lastResult = queryDocumentSnapshots.documents[queryDocumentSnapshots.size() - 1]
+                }
+            }
+    }
+}
