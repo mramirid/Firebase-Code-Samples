04ccd68e474b0d5c27b00861912a793556610a60
Amir <amir.rhythm@gmail.com>
2020-01-11 10:37:13 +0700

    Added PART 2

---
+++ FirebaseUI Firestore/PART 2 - Layouts & Model Class/app/src/main/java/com/mramirid/firebaseui_firestoreexample/Note.kt
@@ -0,0 +1,7 @@
+package com.mramirid.firebaseui_firestoreexample
+
+data class Note(
+    private val title: String? = null,
+    private val description: String? = null,
+    private val priority: Int? = null
+)
